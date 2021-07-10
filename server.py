320b906da174f27039e460ddfd90fda81772650d
Xplosion <syrf.pro@gmail.com>
2021-07-10 06:50:32 +0300

    first commit

---
+++ server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import http.server as SimpleHTTPServer
+import socketserver as SocketServer
+import logging
+import logging.handlers
+import os
+
+
+header_ip = os.getenv('header_ip')
+PORT = 5375
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        logging.handlers.RotatingFileHandler('server.log',
+                            maxBytes=1024*10,
+                            backupCount=15),
+        logging.StreamHandler()
+    ]
+)
+
+class GetHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    def do_GET(self):
+        logging.INFO(self.headers)
+        SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+
+
+logging.info('Starting...')
+Handler = GetHandler
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+httpd.serve_forever()
